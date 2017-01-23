c4cdb02d9e228c0c4fb6cf0f79ba75741df0cf29
Ilya Kantor <iliakan@gmail.com>
2017-01-23 13:45:11 +0300

    github on frontpage

--- scripts/convert_icons.sh
+++ scripts/convert_icons.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3.4 ./woff2otf/woff2otf.py ../styles/blocks/font/icons.woff ../styles/blocks/font/icons.otf
+python3.4 ./woff2otf/woff2otf.py ../modules/styles/blocks/font/icons.woff ../modules/styles/blocks/font/icons.otf
